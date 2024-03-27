207d668c1c39fd997dcfd0ed8aeebe63136acf28
ewrika <egorkaomsk_2003@mail.ru>
2024-03-28 01:33:07 +0600

    Initial Commit

---
+++ ReWriteWeather/ReWriteWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReWriteWeatherApp.swift
+//  ReWriteWeather
+//
+//  Created by Георгий Борисов on 28.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ReWriteWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
